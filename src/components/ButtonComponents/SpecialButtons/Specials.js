9f6ae0f91614c3fec2b2a90b57687adb22000396
gspayonk <grissobel.castro@gmail.com>
2019-09-10 15:57:51 -0400

    Step 1 complete

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -5,13 +5,14 @@ import React from "react";
 //Import your array data to from the provided data file
 
 const Specials = () => {
-  // STEP 2 - add the imported data to state
+    // STEP 2 - add the imported data to state
 
-  return (
-    <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
-    </div>
-  );
+    return ( <
+        div > {
+            /* STEP 3 - Use .map() to iterate over your array data and return a button
+                  component matching the name on the provided file. Pass
+                  it any props needed by the child component*/
+        } <
+        /div>
+    );
 };
--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render( < App / > , document.getElementById("root"));
+
+{ //info on index.js on today's project******
+    //const rootElement = document.getElementById("root");
+    //ReactDOM.render(<App />, rootElement); }
